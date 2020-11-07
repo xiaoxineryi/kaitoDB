640e67cec048b73d8ce1578e29a3ba313f882bcc
xiaoxineryi <529086017@qq.com>
2020-11-08 00:46:27 +0800

    完成了数据库初始化的工作

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+mod pg;
+mod global;
+
+use pg::pg_setting::pg_database as pg_database;
+use crate::global::Global;
+
+
 fn main() {
-    println!("Hello, world!");
+    let a = pg_database::new();
+    a.print();
+    unsafe {
+        global::Global::init_global(None);
+        let id = Global::get_and_add_oid();
+        Global::print();
+    }
+
 }
